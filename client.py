c111b092a2f838e029338459ebea036e152136c7
Ivan Pushkin <pushkin13@bk.ru>
2018-05-13 14:31:12 +0300

    Add argument for simple client

--- client.py
+++ client.py
@@ -1,11 +1,20 @@
-
+from sys import argv
 from websocket import create_connection
-ws = create_connection("ws://localhost:8080/games/0/ws")
+
+group = "0"
+
+if len(argv) > 1:
+    group = argv[1]
+
+url = "ws://localhost:8080/games/{}/ws".format(group)
+print(url)
+
+ws = create_connection(url)
 
 while True:
     try:
         result =  ws.recv()
-        print("Received '%s'".format(result))
+        print("Received '{}'".format(result))
     except KeyboardInterrupt:
         break
 
